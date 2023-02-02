27a0f91ac97f23c8c997a174bf9d90de3819f8d4
raej15 <jonessara141@gmail.com>
2023-02-01 23:52:17 -0500

    user interaction added

--- prog1-extractor/src/prog1.java
+++ prog1-extractor/src/prog1.java
@@ -10,111 +10,123 @@ import java.io.PrintWriter;
  */
 public class prog1 {
    public static void main(String[] args) {
-    
-        try {
-            int cdcLines = 0;
-            int cdcChars = 0;
-            int cdcWordCount =0;
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("What disease are you looking for information about? (input Scabies)");
+        if (keyboard.nextLine().equalsIgnoreCase("scabies")) {
+                
+            try {
+                int cdcLines = 0;
+                int cdcChars = 0;
+                int cdcWordCount =0;
 
-            File cdcData = new File("./prog1-extractor/data/scabies-CDC.txt");
-            Scanner fileReader = new Scanner(cdcData);
-            PrintWriter cdcOutput = new PrintWriter("./prog1-extractor/test/cdcOutput.txt");
+                File cdcData = new File("./prog1-extractor/data/scabies-CDC.txt");
+                Scanner fileReader = new Scanner(cdcData);
+                PrintWriter cdcOutput = new PrintWriter("./prog1-extractor/test/cdcOutput.txt");
 
-            cdcOutput.println("The CDC Reports:\n");
+                cdcOutput.println("The CDC Reports:\n");
 
-            while (fileReader.hasNextLine()) {
-            
-                String data = fileReader.nextLine();
-                if (data.length() > 0) {
-                    if (!(data.substring(data.length() - 1, data.length()).equals(":")) & !(data.substring(data.length() - 1, data.length()).equals("?"))) {
-                        cdcOutput.println("\t" + data);
+                while (fileReader.hasNextLine()) {
+                
+                    String data = fileReader.nextLine();
+                    if (data.length() > 0) {
+                        if (!(data.substring(data.length() - 1, data.length()).equals(":")) & !(data.substring(data.length() - 1, data.length()).equals("?"))) {
+                            cdcOutput.println("\t" + data);
+                        }
+                        else {
+                            cdcOutput.println(data);
+                        }
                     }
-                    else {
-                        cdcOutput.println(data);
-                    }
-                }
-                cdcLines++;
+                    cdcLines++;
 
-                if (data.length() > 0) {
-                    cdcWordCount++;
-                }
-
-                for(int i=0; i < data.length(); i++) {
-                    cdcChars++;
-
-                    if (data.substring(i, i+1).equals(" ") ) {
+                    if (data.length() > 0) {
                         cdcWordCount++;
-                    } 
+                    }
+
+                    for(int i=0; i < data.length(); i++) {
+                        cdcChars++;
+
+                        if (data.substring(i, i+1).equals(" ") ) {
+                            cdcWordCount++;
+                        } 
+                    }
+                    
+
                 }
                 
-
+                cdcOutput.println("\nLines in original file: " + cdcLines);
+                cdcOutput.println("Words in original file: " + cdcWordCount);
+                cdcOutput.println("Chars in original file: " + cdcChars);
+                
+                cdcOutput.close();
+                fileReader.close();
             }
-            
-            cdcOutput.println("\nLines in original file: " + cdcLines);
-            cdcOutput.println("Words in original file: " + cdcWordCount);
-            cdcOutput.println("Chars in original file: " + cdcChars);
-            
-            cdcOutput.close();
-            fileReader.close();
-        }
 
-        catch (FileNotFoundException e) {
-            System.out.println("no file");
-            e.printStackTrace();
-        } 
+            catch (FileNotFoundException e) {
+                System.out.println("no file");
+                e.printStackTrace();
+            } 
 
-        try {
-            int webMDLines = 0;
-            int webMDChars = 0;
-            int webMDWordCount =0;
-            //String cdcData;
+            try {
+                int webMDLines = 0;
+                int webMDChars = 0;
+                int webMDWordCount =0;
+                //String cdcData;
 
-            File webMDData = new File("./prog1-extractor/data/scabies-webMD.txt");
-            Scanner fileReader = new Scanner(webMDData);
-            PrintWriter webMDOutput = new PrintWriter("./prog1-extractor/test/webMDOutput.txt");
+                File webMDData = new File("./prog1-extractor/data/scabies-webMD.txt");
+                Scanner fileReader = new Scanner(webMDData);
+                PrintWriter webMDOutput = new PrintWriter("./prog1-extractor/test/webMDOutput.txt");
 
-            webMDOutput.println("WebMD Reports:\n");
+                webMDOutput.println("WebMD Reports:\n");
 
-            while (fileReader.hasNextLine()) {
-            
-                String data = fileReader.nextLine();
-                if (data.length() > 0) {
-                    if (!(data.substring(data.length() - 1, data.length()).equals(":")) & !(data.substring(data.length() - 1, data.length()).equals("?"))) {
-                        webMDOutput.println("\t" + data);
+                while (fileReader.hasNextLine()) {
+                
+                    String data = fileReader.nextLine();
+                    if (data.length() > 0) {
+                        if (!(data.substring(data.length() - 1, data.length()).equals(":")) & !(data.substring(data.length() - 1, data.length()).equals("?"))) {
+                            webMDOutput.println("\t" + data);
+                        }
+                        else {
+                            webMDOutput.println(data);
+                        }
                     }
-                    else {
-                        webMDOutput.println(data);
-                    }
-                }
-                webMDLines++;
+                    webMDLines++;
 
-                if (data.length() > 0) {
-                    webMDWordCount++;
-                }
-
-                for(int i=0; i < data.length(); i++) {
-                    webMDChars++;
-
-                    if (data.substring(i, i+1).equals(" ") ) {
+                    if (data.length() > 0) {
                         webMDWordCount++;
-                    } 
+                    }
+
+                    for(int i=0; i < data.length(); i++) {
+                        webMDChars++;
+
+                        if (data.substring(i, i+1).equals(" ") ) {
+                            webMDWordCount++;
+                        } 
+                    }
+                    
+
                 }
                 
-
+                webMDOutput.println("\nLines in original file: " + webMDLines);
+                webMDOutput.println("Words in original file: " + webMDWordCount);
+                webMDOutput.println("Chars in original file: " + webMDChars);
+                
+                webMDOutput.close();
+                fileReader.close();
             }
-            
-            webMDOutput.println("\nLines in original file: " + webMDLines);
-            webMDOutput.println("Words in original file: " + webMDWordCount);
-            webMDOutput.println("Chars in original file: " + webMDChars);
-            
-            webMDOutput.close();
-            fileReader.close();
+
+            catch (FileNotFoundException e) {
+                System.out.println("no file");
+                e.printStackTrace();
+            } 
+            System.out.println("Successfully wrote output files.");
         }
 
-        catch (FileNotFoundException e) {
-            System.out.println("no file");
-            e.printStackTrace();
-        } 
+        else {
+            System.out.println("I do not have any information about that. Try asking about scabies next time. \nEXITING PROGRAM");
+            System.exit(0);
+        }
+
+
         
     } 
 
