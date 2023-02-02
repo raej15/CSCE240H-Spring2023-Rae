345db39e3c22366675f5d96beb56c691b015fb03
raej15 <jonessara141@gmail.com>
2023-02-01 22:27:49 -0500

    correctly reads and counts first file

--- prog1-extractor/src/prog1.java
+++ prog1-extractor/src/prog1.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+//import Files.Lines;
 
 /**
  * Takes diseases as input, reports stats, and writes info to output file
@@ -8,18 +9,40 @@ import java.util.Scanner;
  */
 public class prog1 {
    public static void main(String[] args) {
-    //System.out.println(directory.getAbsolutePath());
-
-        System.out.println("The CDC Reports:\n");
+    System.out.println("The CDC Reports:\n");
         try {
+            int cdcLines = 0;
+            int cdcChars = 0;
+            int cdcWordCount =0;
+            //String cdcData;
+
             File cdcData = new File("./prog1-extractor/data/scabies-CDC.txt");
-            Scanner myReader = new Scanner(cdcData);
-            //System.out.println(directory.getAbsolutePath());
-            while (myReader.hasNextLine()) {
-                String data = myReader.nextLine();
+            Scanner fileReader = new Scanner(cdcData);
+
+            while (fileReader.hasNextLine()) {
+                String data = fileReader.nextLine();
                 System.out.println(data);
+                cdcLines++;
+
+                if (data.length() > 0) {
+                    cdcWordCount++;
+                }
+
+                for(int i=0; i < data.length(); i++) {
+                    cdcChars++;
+
+                    if (data.substring(i, i+1).equals(" ") ) {
+                        cdcWordCount++;
+                    } 
+                }
+
             }
-            myReader.close();
+            
+            System.out.println("\nLines in file: " + cdcLines);
+            System.out.println("Words in file: " + cdcWordCount);
+            System.out.println("Chars in file: " + cdcChars);
+            
+            fileReader.close();
         }
 
         catch (FileNotFoundException e) {
@@ -27,23 +50,23 @@ public class prog1 {
             e.printStackTrace();
         } 
 
-        System.out.println("\n\nWebMD Reports:\n");
+     /*   System.out.println("\n\nWebMD Reports:\n");
         try {
             File cdcData = new File("./prog1-extractor/data/scabies-webMD.txt");
-            Scanner myReader = new Scanner(cdcData);
+            Scanner fileReader = new Scanner(cdcData);
             //System.out.println(directory.getAbsolutePath());
-            while (myReader.hasNextLine()) {
-                String data = myReader.nextLine();
+            while (fileReader.hasNextLine()) {
+                String data = fileReader.nextLine();
                 System.out.println(data);
             }
-            myReader.close();
+            fileReader.close();
         }
 
         catch (FileNotFoundException e) {
             System.out.println("no file");
             e.printStackTrace();
         } 
-
+        */
         
     } 
 
