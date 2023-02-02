2e57ca0c1d276625e4b8809b7fb60e8e50c8dffd
raej15 <jonessara141@gmail.com>
2023-02-01 23:09:51 -0500

    successfully writing first file to output

--- prog1-extractor/src/prog1.java
+++ prog1-extractor/src/prog1.java
@@ -1,15 +1,17 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.io.PrintWriter;
 //import Files.Lines;
 
 /**
  * Takes diseases as input, reports stats, and writes info to output file
+ * Used Programiz as a source for writing to files easily: https://www.programiz.com/java-programming/printwriter
  * @author Rae Jones
  */
 public class prog1 {
    public static void main(String[] args) {
-    System.out.println("The CDC Reports:\n");
+    
         try {
             int cdcLines = 0;
             int cdcChars = 0;
@@ -18,10 +20,14 @@ public class prog1 {
 
             File cdcData = new File("./prog1-extractor/data/scabies-CDC.txt");
             Scanner fileReader = new Scanner(cdcData);
+            PrintWriter output = new PrintWriter("output.txt");
+
+            output.println("The CDC Reports:\n");
 
             while (fileReader.hasNextLine()) {
+            
                 String data = fileReader.nextLine();
-                System.out.println(data);
+                output.println(data);
                 cdcLines++;
 
                 if (data.length() > 0) {
@@ -35,13 +41,15 @@ public class prog1 {
                         cdcWordCount++;
                     } 
                 }
+                
 
             }
             
-            System.out.println("\nLines in file: " + cdcLines);
-            System.out.println("Words in file: " + cdcWordCount);
-            System.out.println("Chars in file: " + cdcChars);
+            output.println("\nLines in file: " + cdcLines);
+            output.println("Words in file: " + cdcWordCount);
+            output.println("Chars in file: " + cdcChars);
             
+            output.close();
             fileReader.close();
         }
 
