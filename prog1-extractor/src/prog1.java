2832a284317a519b748416bf0b31f9b21d432e3c
raej15 <jonessara141@gmail.com>
2023-02-01 19:53:10 -0500

    CDC and WebMD functionality added

--- prog1-extractor/src/prog1.java
+++ prog1-extractor/src/prog1.java
@@ -10,9 +10,26 @@ public class prog1 {
    public static void main(String[] args) {
     //System.out.println(directory.getAbsolutePath());
 
-
+        System.out.println("The CDC Reports:\n");
         try {
             File cdcData = new File("./prog1-extractor/data/scabies-CDC.txt");
+            Scanner myReader = new Scanner(cdcData);
+            //System.out.println(directory.getAbsolutePath());
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                System.out.println(data);
+            }
+            myReader.close();
+        }
+
+        catch (FileNotFoundException e) {
+            System.out.println("no file");
+            e.printStackTrace();
+        } 
+
+        System.out.println("\n\nWebMD Reports:\n");
+        try {
+            File cdcData = new File("./prog1-extractor/data/scabies-webMD.txt");
             Scanner myReader = new Scanner(cdcData);
             //System.out.println(directory.getAbsolutePath());
             while (myReader.hasNextLine()) {
