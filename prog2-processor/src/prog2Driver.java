3a3b718d2f1737f0bfe9bc1b309aa5220e0ee92a
raej15 <jonessara141@gmail.com>
2023-02-20 19:25:39 -0500

    reading questions and answers in

--- prog2-processor/src/prog2Driver.java
+++ prog2-processor/src/prog2Driver.java
@@ -3,15 +3,28 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.io.PrintWriter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.ArrayList;
 
-public class prog2 {
+//make sure to credit w3schools for pattern matching
+
+public class prog2Driver {
    public static void main(String[] args) {
     Scanner keyboard = new Scanner(System.in);
     String cdcInfo = "";
-    //System.out.println("do you have gay disease???/");
-    //String userInput = keyboard.nextLine();
+    String currQuestion;
+    String currAnswer;
+    String currLine;
+    Pattern pattern = Pattern.compile("^[A-Z]", Pattern.CASE_INSENSITIVE);
+    Matcher matcher;
+    boolean matchFound;
+    int currIndex = -1;
 
-   // System.out.println(userInput);
+
+    ArrayList<QnA> qnAs = new ArrayList<QnA>();
+    QnA currQnA;
+
 
     try {
 
@@ -21,12 +34,37 @@ public class prog2 {
 
         while (fileReader.hasNextLine()) {
         
-            cdcInfo = cdcInfo + fileReader.nextLine() + "\n";
+            //cdcInfo = cdcInfo + fileReader.nextLine() + "\n";
+            currLine = fileReader.nextLine();
+            matcher = pattern.matcher(currLine);     
+            matchFound = matcher.find();
+            
+            currQnA = new QnA();
+
+            if(matchFound) {
+                //this is a question
+                //create a new QnA object; add question to it
+                //create new object w linked list here
+                qnAs.add(currQnA);
+                currIndex++;
+
+                qnAs.get(currIndex).setQuestion(currLine);
+                System.out.println("question found");
+            } else {
+                ///this is an answer
+                //add line to current answer
+                qnAs.get(currIndex).addToAnswer(currLine);
+                System.out.println("answer found");
+            }
+          
         }
 
-       // System.out.print(cdcInfo);
+       //System.out.print(cdcInfo);
     
         fileReader.close();
+        qnAs.get(1).print();
+        qnAs.get(2).print();
+        qnAs.get(3).print();
     }
 
     catch (FileNotFoundException e) {
@@ -34,6 +72,8 @@ public class prog2 {
         e.printStackTrace();
     }
 
+    
+    /* 
     //System.out.println("What questions do you have about scabies?");
     String userInput = "What is scabies?";
     //keyboard.nextLine();
@@ -46,15 +86,8 @@ public class prog2 {
 
         
 
-    } 
-    
-    
-
-    /*for (int i = 0; i<cdcInfo.length(); i++) {
-        if (cdcInfo.startsWith("\n\t")) {
-
-       }
     } */
+    
 
    } 
     
