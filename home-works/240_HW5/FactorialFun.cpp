fdd500d82830c03ac1ada99317cc4515d049d0ac
raej15 <jonessara141@gmail.com>
2023-03-14 20:51:46 -0400

    some error handling

--- home-works/240_HW5/FactorialFun.cpp
+++ home-works/240_HW5/FactorialFun.cpp
@@ -17,14 +17,30 @@ using namespace std::chrono;
 
 int getFactorial(int n) {
 	int factorial = 1;
-	for(int i = 1; i <= n; ++i) {
+	for(int i = 2; i <= n; i++) {
             factorial *= i;
+			cout << factorial << endl;
     }
 	return factorial;
 }
 
+int validateNum(int n) {
+	if(0>= n |  n >= 10) {
+		cout << "Invalid Reponse for N\n- - - - - - - - - - - - - - - - - - - - - - - - - \nEXITING PROGRAM" << endl;
+		return 0;
+	}
+}
+
+int validateR(int r, int n) {
+	if(0>= r |  r >= 10 | r>=n) {
+		cout << "Invalid Reponse for r\n- - - - - - - - - - - - - - - - - - - - - - - - - \nEXITING PROGRAM" << endl;
+		return 0;
+	}
+}
+
 int main() {
-		int num,r;
+		int num;
+		int r;
 		//int factorial = 1;
 		int choice = 0;
 		int numF = 0;
@@ -40,15 +56,27 @@ int main() {
 		system("clear");
 
 		if (choice == 1){
-			cout << "Enter a positive whole number" << endl;
+			cout << "Enter a positive whole number less than 12" << endl;
 			cin >> num;
+			if(0>= num |  num >= 10) {
+				cout << "Invalid Reponse\n- - - - - - - - - - - - - - - - - - - - - - - - - \nEXITING PROGRAM" << endl;
+				return 0;
+			}			
 			numF = getFactorial(num);
 			system("clear");
 			cout << "- - - - - - - - - - - - - - - - - - - - - - - - - \nYour number: " << num << "\nYour factorial: " << numF << endl;
 		}
 		else {
-			cout << "Enter 2 positive whole numbers separated by a space\n the first number must be bigger than the second" << endl;
+			cout << "Enter 2 positive whole numbers less than 12, separated by a space\n the first number must be bigger than the second" << endl;
 			cin >> num >> r;
+			if(0>= num |  num >= 10) {
+				cout << "Invalid Reponse\n- - - - - - - - - - - - - - - - - - - - - - - - - \nEXITING PROGRAM" << endl;
+				return 0;
+			}
+			if(0>= r |  r >= 10 | r>=num) {
+				cout << "Invalid Reponse\n- - - - - - - - - - - - - - - - - - - - - - - - - \nEXITING PROGRAM" << endl;
+				return 0;
+			}
 			numF = getFactorial(num);
 			numR = getFactorial(r);
 			subCom = getFactorial(num - r);
@@ -70,7 +98,7 @@ int main() {
 	    // use duration cast method
 	    auto duration = duration_cast<microseconds>(stop - start);
 
-	    cout << "\nProcessing Time: " << duration.count() << " microseconds" << "\n- - - - - - - - - - - - - - - - - - - - - - - - - " << endl;
+	    cout << "\nProcessing Time: " << duration.count() << " microseconds" << "\n- - - - - - - - - - - - - - - - - - - - - - - - - \nEXITING PROGRAM" << endl;
 
 	    return 0;
 }
