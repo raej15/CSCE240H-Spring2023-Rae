c7be0a2861c2a9969a23d37e9886def61fa054bc
raej15 <jonessara141@gmail.com>
2023-03-24 21:46:00 -0400

    confidence counter working

--- prog4-userintent2querymapper/src/Response.java
+++ prog4-userintent2querymapper/src/Response.java
@@ -1,11 +1,21 @@
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+/**
+ * 
+ * Credit: https://stackoverflow.com/questions/7929495/java-finding-the-number-of-word-matches-in-a-given-string for pattern matching in confidence estimation
+ */
 public class Response {
-    /*
+    
     public static void main(String[] args) {
-       System.out.println(handleQuerys(" poop"));
+       //System.out.println(handleQuerys(" poop"));
+       String userInput = "what is the disease Scabies";
+       System.out.println(queryConfidence(userInput));
+
+
     }
-    */
+    
     
     //static ArrayList<QnA> qnAs = new ArrayList<QnA>();
 
@@ -24,16 +34,43 @@ public class Response {
     }
 
     */
+    public static String[][] knownQueries = {{"What is Scabies", "Who is at Risk", "What can travelers do to prevent scabies", "Types of Scabies", "Symptoms", "When to call a Doctor", "Scabies Diagnosis", "Scabies Treatment", "Scabies Complications", "Tell me everything"},{"What is", "Who", "travel"}};
 
-    public static String chooseQuery(String userInput) {
-        
+
+    public static String chosenQuery(String userInput, ArrayList<QnA> qnAs) {
+        //int[][] myNumbers = { {1, 2, 3, 4}, {5, 6, 7} };
+        String closestQuery;
+        int matchNum;
+        //String[] uSplit = userInput.split("\\s");
+        String[] qSplit;
+
     }
+
+    public static int queryConfidence(String userInput) {
+        String[] uSplit = userInput.split("\\s");
+        int totalMatches = 0;
+         for ( int i = 0; i < uSplit.length; i++) {
+             totalMatches= totalMatches + queryMatch(uSplit[i], "What is Scabies");
+ 
+         }
+         return totalMatches;
+    }
+
+    public static int queryMatch(String userInputWord, String knownQuery) {
+        String toMatch = "\\b" + userInputWord + "\\b";
+        int matches = 0;  
+        Matcher matcher = Pattern.compile(toMatch, Pattern.CASE_INSENSITIVE).matcher(knownQuery);
+        while (matcher.find()) matches++;
+        return matches;
+
+    }
+
 
     public static String handleQuerys(String query, ArrayList<QnA> qnAs) {
         String answer = ""; 
 
         for(int i = 0; i < qnAs.size(); i++) {
-            if (qnAs.get(i).getQuestion().contains(query) | qnAs.get(i).getAnswer().contains(query)){
+            if (qnAs.get(i).getQuestion().contains(query) /* | qnAs.get(i).getAnswer().contains(query)*/){
                 answer = answer + "\n" + qnAs.get(i).getAnswer();
             }
         }
