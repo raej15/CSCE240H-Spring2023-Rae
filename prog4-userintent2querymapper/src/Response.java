d46cca4818c7a1469fef473d752c4cbac2a04737
raej15 <jonessara141@gmail.com>
2023-03-24 22:41:44 -0400

    handles bad input

--- prog4-userintent2querymapper/src/Response.java
+++ prog4-userintent2querymapper/src/Response.java
@@ -46,7 +46,7 @@ public class Response {
 
 
     public static String chosenQuery(String userInput) {
-        String[][] knownQueries = {{"What is Scabies", "Who is at Risk", "What can travelers do to prevent scabies", "Types of Scabies", "Symptoms", /*"When to call a Doctor",*/ "Scabies Diagnosis", "Scabies Treatment", "Scabies Complications", "Tell me everything"},{"What is", "Who", "travel", "Crusted", "ymptoms", "Diagnosing", "Treating", "Complications", "everything"}};
+        String[][] knownQueries = {{"What is Scabies", "Who is at Risk", "What can travelers do to prevent scabies", "Type of Scabies", "ympt", /*"When to call a Doctor",*/ "Scabies Diag", "Scabies Treat", "Scabies ompli", "Tell me everything"},{"What is", "Who", "travel", "Crusted", "ymptoms", "Diagnosing", "Treating", "Complications", "everything"}};
         //int[][] myNumbers = { {1, 2, 3, 4}, {5, 6, 7} };
         String closestQuery = knownQueries[0][0];
         int highMatchNum = 0;
@@ -70,7 +70,13 @@ public class Response {
         //for (int i=0; i < 10; i++) {
          //   System.out.println(queryConfidence(userInput, knownQueries[i][0]));
         //}
-        return closestQuery;
+        if (highMatchNum==0) {
+            return "";
+        }
+
+        else {
+            return closestQuery;
+        }
 
     }
 
@@ -99,7 +105,9 @@ public class Response {
     public static String handleQuerys(/*String query, */ String userInput, ArrayList<QnA> qnAs) {
         String query = chosenQuery(userInput);
         String answer = ""; 
-
+        if (query == "") {
+            return "I do not know this information. Try asking something different, or in a different way.\nPlease check that you havent misspelled your question.";
+        }
         for(int i = 0; i < qnAs.size(); i++) {
             if (qnAs.get(i).getQuestion().contains(query) /* | qnAs.get(i).getAnswer().contains(query)*/){
                 answer = answer + "\n" + qnAs.get(i).getAnswer();
