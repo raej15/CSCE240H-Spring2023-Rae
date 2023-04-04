4ee9d2cccdf3b34d6a24cad4350e04a0f3a155fb
raej15 <jonessara141@gmail.com>
2023-04-03 23:47:00 -0400

    progress on UI

--- prog5-sessionlogger/src/session analyzing/statsUI.java
+++ prog5-sessionlogger/src/session analyzing/statsUI.java
@@ -8,32 +8,45 @@ public class statsUI {
     public static void main(String[] args) {
 
         Scanner keyboard = new Scanner(System.in);
-        int userInput;
-        int uniqueUserInput;
+        int userInput = 0;
+        int uniqueUserInput = 0;
 
-        System.out.println("Would you like to \n1. get overall summary\n2. look at a specific chat's data");
-        userInput = keyboard.nextInt();
-        if (userInput == 1) {
-            System.out.println(sessionData.totalSummary());
-        }
-        else if (userInput==2) {
-            System.out.println("Enter the chat # you'd like to see");
+        while (!(userInput == 3)) {
+            System.out.println("\n- - - - - - - - - - - - - - - - - - - - - -\n\nHOME MENU:\n\nWould you like to \n1. get overall summary\n2. look at a specific chat's data\n3. quit");
             userInput = keyboard.nextInt();
-            if (sessionData.checkSessionValid(userInput)) {
-                System.out.println("Would you like to \n1. show chat log\n2. look at chat's summary");
-                uniqueUserInput = keyboard.nextInt();
-                if (uniqueUserInput == 1) {
-                    sessionData.sessionFileName(userInput);
+            if (userInput == 1) {
+                System.out.println(sessionData.totalSummary());
+            }
+            else if (userInput==2) {
+                System.out.println("\n- - - - - - - - - - - - - - - - - - - - - -\n\nEnter the chat # you'd like to see");
+                userInput = keyboard.nextInt();
+                if (sessionData.checkSessionValid(userInput)) {
+                    System.out.println("\n- - - - - - - - - - - - - - - - - - - - - -\n\nWould you like to \n1. show chat log\n2. look at chat's summary\n3. quit");
+                    uniqueUserInput = keyboard.nextInt();
+                    if (uniqueUserInput == 1) {
+                        sessionData.sessionFileName(userInput);
+                    }
+                    else if (uniqueUserInput == 2) {
+                        System.out.println("\n- - - - - - - - - - - - - - - - - - - - - -\n\nChat " + userInput + "'s Session Summary:\n\n" + sessionData.sessionSummary(userInput));
+                    }
+                    else if (uniqueUserInput == 3) {
+                        System.out.println("EXITING PROGRAM");
+                        System.exit(0);
+                    }
                 }
-                else if (uniqueUserInput == 2) {
-                    sessionData.sessionSummary(userInput);
+                else {
+                    System.out.println("\n- - - - - - - - - - - - - - - - - - - - - -\n\nERROR: there are only " + sessionData.sessionCounter() + " chat sessions. Please choose a valid number.");
                 }
             }
-            else {
-                System.out.println("ERROR: there are only " + sessionData.sessionCounter() + " chat sessions. Please choose a valid number.");
-            }
-        }
 
-    }
+            else if (userInput==3) {
+                System.out.println("EXITING PROGRAM");
+                System.exit(0);
+            }
+
+
+
+        }
     
+    }
 }
